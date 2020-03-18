e3a80fdd4948aa2e91e4914e2a83210fd449a973
执念 <gaomingwei2017@163.com>
2020-03-18 08:07:19 +0800

    fix bug

--- js/view/Personal/PersonalPage.js
+++ js/view/Personal/PersonalPage.js
@@ -1,46 +1,56 @@
-'use strict'
-import * as React from 'react'
-import UserInfo from './UserInfo'
-import {connect} from 'react-redux'
-import actions from '../../redux/actions'
-import {View,Text,ScrollView,StyleSheet,SafeAreaView,TouchableOpacity} from 'react-native'
-import {flex,row,defaultBackgroundColor, center, iosFontFmily} from '../../styles/constants'
-import PersonalItem from './components/PersonlItem'
-import {screentWidth} from '../../utils/screenUtil'
+import * as React from 'react';
+import UserInfo from './UserInfo';
+import {connect} from 'react-redux';
+import actions from '../../redux/actions';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  flex,
+  row,
+  defaultBackgroundColor,
+  center,
+  iosFontFmily,
+} from '../../styles/constants';
+import PersonalItem from './components/PersonlItem';
+import {screentWidth} from '../../utils/screenUtil';
 
 class PersonalPage extends React.Component {
   async componentDidMount() {}
-  handleExit=()=> {
-
-  }
+  handleExit = () => {};
   // 退出
   _exit() {
     return (
-      <TouchableOpacity onPress={this.handleExit} activeOpacity={0.8} style={styles.exitBox}>
+      <TouchableOpacity
+        onPress={this.handleExit}
+        activeOpacity={0.8}
+        style={styles.exitBox}>
         <Text style={styles.exitText}>退出登录</Text>
       </TouchableOpacity>
-    )
+    );
   }
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView>
-          <UserInfo/>
-          <PersonalItem/>
+          <UserInfo />
+          <PersonalItem />
           {this._exit()}
         </ScrollView>
       </SafeAreaView>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({})
-const mapDispatchToProps = dispatch => ({})
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PersonalPage)
+export default connect(mapStateToProps, mapDispatchToProps)(PersonalPage);
 
 const styles = StyleSheet.create({
   container: {
@@ -57,5 +67,5 @@ const styles = StyleSheet.create({
     fontFamily: iosFontFmily,
     fontSize: 20,
     color: 'red',
-  }
-})
+  },
+});
